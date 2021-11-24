aa7a07b95f7590b04f6197408223d4ede471ecd4
Scott Twiname <skott.twiname@gmail.com>
2021-11-25 12:02:43 +1300

    Reload project when meta or deployments updated

--- src/hooks/useProject.tsx
+++ src/hooks/useProject.tsx
@@ -1,16 +1,55 @@
 // Copyright 2020-2021 OnFinality Limited authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { useIPFS, useProjectMetadata, useQueryRegistry } from '../containers';
+import { useContracts, useIPFS, useProjectMetadata, useQueryRegistry } from '../containers';
 import { ProjectDetails } from '../models';
 import { AsyncData } from '../utils';
 import { getDeployment } from './useDeployment';
 import { useAsyncMemo } from '.';
+import { useCallback, useEffect, useState } from 'react';
+import { BigNumber } from 'ethers';
 
 export function useProject(id: string): AsyncData<ProjectDetails | undefined> {
   const { getQuery } = useQueryRegistry();
   const { catSingle } = useIPFS();
   const { getMetadataFromCid } = useProjectMetadata();
+  const pendingContracts = useContracts();
+
+  // Used to rerun async memo
+  const [cacheBreak, setCacheBreak] = useState<number>(0);
+
+  const sub = useCallback(async () => {
+    if (!pendingContracts) {
+      return () => undefined;
+    }
+
+    const contracts = await pendingContracts;
+    const listener = (owner: string, queryId: unknown) => {
+      /* TODO need updated query with indexed params*/
+      if ((queryId as BigNumber).toHexString() !== id) {
+        return;
+      }
+      setCacheBreak((val) => val + 1);
+    };
+    const deploymentFilter = contracts?.queryRegistry.filters.UpdateQueryDeployment(/*null, id*/);
+    const metadataFilter = contracts?.queryRegistry.filters.UpdateQueryMetadata(/*null, id*/);
+
+    contracts.queryRegistry.on(deploymentFilter, listener);
+    contracts.queryRegistry.on(metadataFilter, listener);
+
+    return () => {
+      contracts.queryRegistry.off(deploymentFilter, listener);
+      contracts.queryRegistry.off(metadataFilter, listener);
+    };
+  }, [pendingContracts, id]);
+
+  useEffect(() => {
+    const pendingUnsub = sub();
+
+    return () => {
+      pendingUnsub.then((unsub) => unsub());
+    };
+  }, [sub]);
 
   return useAsyncMemo(async () => {
     if (!id) {
@@ -28,8 +67,9 @@ export function useProject(id: string): AsyncData<ProjectDetails | undefined> {
     return {
       id,
       owner: query.owner,
+      version: query.version,
       metadata,
       deployment,
     };
-  }, [id, catSingle, getMetadataFromCid, getQuery]);
+  }, [id, catSingle, getMetadataFromCid, getQuery, cacheBreak]);
 }
