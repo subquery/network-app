8b4f7f36b45e613770ec8d72e50b58515081b56c
Scott Twiname <skott.twiname@gmail.com>
2021-11-26 17:00:38 +1300

    Fix some stories being broken

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@ import '!style-loader!css-loader!../src/index.css';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 import i18n from './i18next.js';
+import 'reflect-metadata';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
