00a153982b18835715ffdd381ea5b07505353c1d
Scott Twiname <skott.twiname@gmail.com>
2022-03-16 15:33:26 +1300

    Update rewards table to antd

--- src/pages/staking/Indexer/Rewards/Rewards.tsx
+++ src/pages/staking/Indexer/Rewards/Rewards.tsx
@@ -4,7 +4,6 @@
 import { BigNumber } from '@ethersproject/bignumber';
 import { formatEther } from '@ethersproject/units';
 import { Spinner, Typography } from '@subql/react-ui';
-import { Table, TableBody, TableCell, TableHead, TableRow } from '@subql/react-ui/dist/components/Table';
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useRewards } from '../../../../containers';
@@ -14,49 +13,7 @@ import {
   GetRewards_unclaimedRewards_nodes as UnclaimedReward,
 } from '../../../../__generated__/GetRewards';
 import ClaimRewards from './ClaimRewards';
-
-const UnclaimedRewardItem: React.VFC<{
-  key: string;
-  reward: UnclaimedReward;
-  onRewardsClaimed?: () => void;
-}> = ({ key, reward }) => {
-  const amount = formatEther(BigNumber.from(reward.amount));
-  return (
-    <TableRow>
-      <TableCell>
-        <Typography>{key}</Typography>
-      </TableCell>
-      <TableCell>
-        <Typography>{reward.indexerAddress}</Typography>
-      </TableCell>
-      <TableCell>
-        <Typography>{`${amount} SQT`}</Typography>
-      </TableCell>
-      <TableCell>
-        <ClaimRewards indexer={reward.indexerAddress} amount={amount} />
-      </TableCell>
-    </TableRow>
-  );
-};
-
-const ClaimedReward: React.VFC<{
-  key: string;
-  reward: Reward;
-}> = ({ key, reward }) => {
-  return (
-    <TableRow>
-      <TableCell>
-        <Typography>{key}</Typography>
-      </TableCell>
-      <TableCell>
-        <Typography>{reward.indexerAddress}</Typography>
-      </TableCell>
-      <TableCell>
-        <Typography>{`${formatEther(BigNumber.from(reward.amount))} SQT`}</Typography>
-      </TableCell>
-    </TableRow>
-  );
-};
+import { Table as TableD, TableProps } from 'antd';
 
 function isClaimedReward(reward: Reward | UnclaimedReward): reward is Reward {
   return !!(reward as Reward).claimedTime;
@@ -65,6 +22,39 @@ function isClaimedReward(reward: Reward | UnclaimedReward): reward is Reward {
 const Rewards: React.VFC<{ delegatorAddress: string }> = ({ delegatorAddress }) => {
   const rewards = useRewards({ address: delegatorAddress });
   const { t } = useTranslation('translation');
+
+  const colums: TableProps<Reward | UnclaimedReward>['columns'] = [
+    {
+      title: '#',
+      key: 'idx',
+      width: 30,
+      render: (t, r, index) => <Typography>{index + 1}</Typography>,
+    },
+    {
+      title: t('rewards.header1').toUpperCase(),
+      dataIndex: 'indexerAddress',
+      key: 'indexer',
+      width: 200,
+      render: (t: string) => <Typography>{t}</Typography>,
+    },
+    {
+      title: t('rewards.header2').toUpperCase(),
+      dataIndex: 'amount',
+      key: 'amount',
+      width: 80,
+      render: (amount: BigInt) => <Typography>{formatEther(BigNumber.from(amount))}</Typography>,
+    },
+    {
+      title: t('rewards.header3').toUpperCase(),
+      dataIndex: 'amount',
+      key: 'action',
+      width: 60,
+      render: (t, reward: Reward | UnclaimedReward) =>
+        isClaimedReward(reward) ? undefined : (
+          <ClaimRewards indexer={reward.indexerAddress} amount={formatEther(BigNumber.from(reward.amount))} />
+        ),
+    },
+  ];
 
   return renderAsyncArray(
     mapAsync(
@@ -78,41 +68,7 @@ const Rewards: React.VFC<{ delegatorAddress: string }> = ({ delegatorAddress }) 
       error: (error) => <Typography>{`Failed to get pending rewards: ${error.message}`}</Typography>,
       loading: () => <Spinner />,
       empty: () => <Typography>{t('rewards.none')}</Typography>,
-      data: (data) => (
-        <>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>
-                  <Typography>#</Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography>{t('rewards.header1')}</Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography>{t('rewards.header2')}</Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography>{t('rewards.header3')}</Typography>
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data.map((reward, index) =>
-                isClaimedReward(reward) ? (
-                  <ClaimedReward reward={reward} key={index.toString()} />
-                ) : (
-                  <UnclaimedRewardItem
-                    reward={reward}
-                    key={index.toString()}
-                    onRewardsClaimed={() => rewards.refetch()}
-                  />
-                ),
-              )}
-            </TableBody>
-          </Table>
-        </>
-      ),
+      data: (data) => <TableD columns={colums} dataSource={data} scroll={{ x: 800 }} />,
     },
   );
 };
