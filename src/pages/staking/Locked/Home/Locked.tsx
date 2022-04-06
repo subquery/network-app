fdbc699a35b5c7efa747b7cecaf1bdf0e5bc7166
CaiYiLiang <cherryliang0112@gmail.com>
2022-04-06 15:49:43 +1200

    add endAt & status to locked table

--- src/pages/staking/Locked/Home/Locked.tsx
+++ src/pages/staking/Locked/Home/Locked.tsx
@@ -2,21 +2,35 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { Spinner, Typography } from '@subql/react-ui';
+import moment from 'moment';
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useWeb3, useWithdrawls } from '../../../../containers';
-import { mapAsync, notEmpty, renderAsyncArray } from '../../../../utils';
+import { useLockPeriod } from '../../../../hooks';
+import { mapAsync, mergeAsync, notEmpty, renderAsyncArray } from '../../../../utils';
 import { LockedList } from '../LockedList';
 
 export const Locked: React.VFC = () => {
   const { t } = useTranslation();
   const { account } = useWeb3();
   const withdrawals = useWithdrawls({ delegator: account || '' });
+  const lockPeriod = useLockPeriod();
 
   return (
     <div className={'contentContainer'}>
       {renderAsyncArray(
-        mapAsync((data) => data.withdrawls?.nodes.filter(notEmpty), withdrawals),
+        mapAsync(
+          ([withdrawlsResult, lockPeriod]) =>
+            withdrawlsResult?.withdrawls?.nodes
+              .map((withdrawal, idx) => {
+                const startAt = moment(withdrawal?.startTime).format();
+                const endAt = moment(startAt).add(lockPeriod, 'second').format();
+                const status = moment().format() > endAt ? t('withdrawals.unlock') : t('withdrawals.lock');
+                return { ...withdrawal, startAt, endAt, status, idx };
+              })
+              .filter(notEmpty),
+          mergeAsync(withdrawals, lockPeriod),
+        ),
         {
           error: (e) => <Typography>{`Error: Fail to get Indexers ${e.message}`}</Typography>,
           loading: () => <Spinner />,
--- src/pages/staking/Locked/LockedList/LockedList.tsx
+++ src/pages/staking/Locked/LockedList/LockedList.tsx
@@ -3,47 +3,76 @@
 
 import { Typography } from '@subql/react-ui';
 import * as React from 'react';
-import { Table, TableHead, TableBody, TableRow, TableCell } from '@subql/react-ui/dist/components/Table';
-import { BigNumber } from 'ethers';
+import { Table, TableProps } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { formatEther } from '../../../../utils';
 import { GetWithdrawls_withdrawls_nodes as Withdrawls } from '../../../../__generated__/GetWithdrawls';
 import styles from './LockedList.module.css';
 import { DoWithdraw } from '../DoWithdraw';
+import moment from 'moment';
 
-//TODO: status: locked, unlock(available to claim)
+const dateFormat = 'MMMM Do YY, h:mm:ss a';
 
+// TODO: Wait for design confirm the finalized tableCell style
 const TableCellText = ({ children }: { children: React.ReactChild | React.ReactChildren }) => (
-  <TableCell>
-    <Typography className={styles.tableCell} variant="medium">
-      {children}
-    </Typography>
-  </TableCell>
+  <Typography className={styles.tableCell} variant="medium">
+    {children}
+  </Typography>
 );
 
+interface SortedWithdrawals extends Partial<Withdrawls> {
+  idx: number;
+  startAt: string;
+  endAt: string;
+  status: string;
+}
 interface props {
-  withdrawals: Withdrawls[];
+  withdrawals: SortedWithdrawals[];
 }
 
 export const LockedList: React.VFC<props> = ({ withdrawals }) => {
   const { t } = useTranslation();
-  // const lockPeriod = useLockPeriod();
 
-  const sortedWithdrawals = withdrawals.map((withdrawal) => {
-    const sortedAmount = formatEther(BigNumber.from(withdrawal.amount));
-    const status = withdrawal.claimed ? t('withdrawals.claimed') : t('withdrawals.unClaim');
-    const startAt = new Date(withdrawal.startTime).toLocaleString();
-    return { amount: sortedAmount, status, startAt };
-  });
+  const columns: TableProps<SortedWithdrawals>['columns'] = [
+    {
+      title: '#',
+      width: 30,
+      align: 'center',
+      render: (t, r, index) => <TableCellText>{index + 1}</TableCellText>,
+    },
+    {
+      title: t('withdrawals.amount').toUpperCase(),
+      dataIndex: 'amount',
+      width: 120,
+      align: 'center',
+      render: (val: string) => <TableCellText>{`${formatEther(val)} SQT`}</TableCellText>,
+    },
 
-  const tableHeaders = [
-    '#',
-    t('withdrawals.amount').toUpperCase(),
-    t('withdrawals.start').toUpperCase(),
-    t('withdrawals.status').toUpperCase(),
+    {
+      title: t('withdrawals.startAt').toUpperCase(),
+      dataIndex: 'startAt',
+      width: 80,
+      align: 'center',
+      render: (val: string) => <TableCellText>{moment(val).format(dateFormat)}</TableCellText>,
+    },
+    {
+      title: t('withdrawals.endAt').toUpperCase(),
+      dataIndex: 'endAt',
+      width: 80,
+      align: 'center',
+      render: (val: string) => <TableCellText>{moment(val).format(dateFormat)}</TableCellText>,
+    },
+    {
+      title: t('withdrawals.status').toUpperCase(),
+      dataIndex: 'status',
+      width: 60,
+      align: 'center',
+      render: (val: string) => <TableCellText>{val}</TableCellText>,
+    },
   ];
 
-  const headerTitle = `${t('withdrawals.unlockedAsset', { count: withdrawals.length || 0 })}`;
+  const availableClaimAmount = withdrawals.filter((withdrawal) => withdrawal.endAt < moment().format()).length;
+  const headerTitle = `${t('withdrawals.unlockedAsset', { count: availableClaimAmount || 0 })}`;
 
   return (
     <div className={styles.container}>
@@ -51,28 +80,10 @@ export const LockedList: React.VFC<props> = ({ withdrawals }) => {
         <Typography variant="h6" className={styles.title}>
           {headerTitle}
         </Typography>
-        <DoWithdraw />
+        {availableClaimAmount > 0 && <DoWithdraw />}
       </div>
 
-      <Table>
-        <TableHead>
-          <TableRow>
-            {tableHeaders.map((header) => (
-              <TableCell key={header}>{header}</TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {sortedWithdrawals.map((withdrawals, idx) => (
-            <TableRow key={idx}>
-              <TableCellText>{idx + 1}</TableCellText>
-              <TableCellText>{withdrawals.amount}</TableCellText>
-              <TableCellText>{withdrawals.startAt}</TableCellText>
-              <TableCellText>{withdrawals.status}</TableCellText>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <Table columns={columns} dataSource={withdrawals} rowKey="idx" />
     </div>
   );
 };
