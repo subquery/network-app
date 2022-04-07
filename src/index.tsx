0311569e538313025e411890e529335a6bb202ea
CaiYiLiang <cherryliang0112@gmail.com>
2022-04-07 16:42:56 +1200

    add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,20 @@
 import 'reflect-metadata';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'antd/dist/antd.css';
 import './index.css';
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
