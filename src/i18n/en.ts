2df2c4d99035353462c127cd24b54e0f463ab6b3
CaiYiLiang <cherryliang0112@gmail.com>
2022-04-08 13:52:38 +1200

    update withdraw modal

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -240,9 +240,10 @@ const en = {
       noWithdrawals: 'No withdrawals available.',
       withdrawToken: 'Withdraw all unlocked assets',
       withdraw: 'Withdraw',
-      confirm: 'Confirm the claim',
+      confirm: 'Confirm withdrawal amount',
       enterWithdrawAmount: 'Enter the amount of SQT you want to withdraw',
       confirmWithdraw: 'Confirm Withdraw',
+      aboutToWithdraw: 'You are about to withdraw {{amount}} SQT.',
     },
     general: {
       current: 'current',
--- src/pages/staking/Locked/DoWithdraw/DoWithdraw.tsx
+++ src/pages/staking/Locked/DoWithdraw/DoWithdraw.tsx
@@ -10,8 +10,13 @@ import { useTranslation } from 'react-i18next';
 import { useContracts } from '../../../../containers';
 import TransactionModal from '../../../../components/TransactionModal';
 import clsx from 'clsx';
+import { T } from 'ramda';
 
-export const DoWithdraw: React.VFC = () => {
+interface DoWithdrawProps {
+  unlockedAmount: number;
+}
+
+export const DoWithdraw: React.VFC<DoWithdrawProps> = ({ unlockedAmount }) => {
   const { t } = useTranslation();
   const pendingContracts = useContracts();
 
@@ -33,11 +38,14 @@ export const DoWithdraw: React.VFC = () => {
     <div>
       <TransactionModal
         text={modalText}
-        actions={[{ label: t('rewards.claim.button'), key: 'claim' }]}
+        actions={[{ label: t('withdrawals.withdraw'), key: 'claim' }]}
         onClick={handleClick}
         renderContent={(onSubmit, _, isLoading, error) => {
           return (
             <>
+              <Typography className={styles.unlockedAmount}>
+                {t('withdrawals.aboutToWithdraw', { amount: unlockedAmount })}
+              </Typography>
               <Typography className={'errorText'}>{error}</Typography>
               <div className={clsx(styles.btnContainer, 'flex-end')}>
                 <Button
