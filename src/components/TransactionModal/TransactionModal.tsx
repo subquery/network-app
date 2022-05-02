ddaf407b60422cc6fb0c7b53a7f3010d7d1f4f83
CaiYiLiang <cherryliang0112@gmail.com>
2022-05-02 13:52:45 +1200

    fix showClock error

--- src/components/TransactionModal/TransactionModal.tsx
+++ src/components/TransactionModal/TransactionModal.tsx
@@ -83,6 +83,7 @@ const TransactionModal = <P, T extends string>({
     setShowModal(undefined);
     setFailureModalText(undefined);
     setSuccessModalText(undefined);
+    !isLoading && setShowClock(false);
   };
 
   const resetModalStatus = () => {
