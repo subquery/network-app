a5bf0f367d6d1f79ada349931a4da41a2c080868
CaiYiLiang <cherryliang0112@gmail.com>
2022-06-10 20:35:15 +1200

    feat: add deploymentInfo for selectedDeploymentId

--- src/pages/plans/MyOffers/CreateOffer/SelectDeployment/SelectDeployment.tsx
+++ src/pages/plans/MyOffers/CreateOffer/SelectDeployment/SelectDeployment.tsx
@@ -8,8 +8,10 @@ import { CreateOfferContext, StepButtons } from '../CreateOffer';
 import styles from './SelectDeployment.module.css';
 import { useHistory } from 'react-router';
 import { EXPLORER_ROUTE } from '../../../../explorer';
-import { SearchInput } from '../../../../../components';
+import { DeploymentInfo, SearchInput, Spinner } from '../../../../../components';
 import { useDeploymentQuery } from '../../../../../containers';
+import { useProject } from '../../../../../hooks';
+import { renderAsync } from '../../../../../utils';
 
 const Description = () => {
   const history = useHistory();
@@ -35,7 +37,9 @@ export const SelectDeployment: React.VFC = () => {
   /**
    * SearchInput logic
    */
-  const [searchDeployment, setSearchDeployment] = React.useState<string | undefined>();
+  const [searchDeployment, setSearchDeployment] = React.useState<string | undefined>(
+    createOfferContext?.offer?.deploymentId,
+  );
   const sortedDeployment = useDeploymentQuery({ deploymentId: searchDeployment ?? '' });
   const searchedDeployment = React.useMemo(() => sortedDeployment.data?.deployment, [sortedDeployment]);
 
@@ -43,7 +47,7 @@ export const SelectDeployment: React.VFC = () => {
     <SearchInput
       onSearch={(value) => setSearchDeployment(value)}
       onChange={(e) => !e.target.value && setSearchDeployment(undefined)}
-      defaultValue={searchDeployment}
+      defaultValue={searchDeployment || createOfferContext?.offer?.deploymentId}
       loading={sortedDeployment.loading}
       emptyResult={!searchedDeployment}
       placeholder={t('myOffers.step_1.search')}
@@ -55,7 +59,34 @@ export const SelectDeployment: React.VFC = () => {
    */
 
   if (!createOfferContext) return <></>;
-  const { curStep, onStepChange, totalSteps } = createOfferContext;
+  const { curStep, onStepChange, totalSteps, updateCreateOffer, offer } = createOfferContext;
+  const onNext = (step: number) => {
+    updateCreateOffer({ ...offer, deploymentId: searchedDeployment?.id });
+    onStepChange(step);
+  };
+
+  const DeploymentProject = ({ projectId }: { projectId: string }) => {
+    const asyncProject = useProject(projectId);
+    return (
+      <div className={styles.deploymentInfoContainer}>
+        <Typography.Title level={5}>{t('myOffers.step_1.selectedId')}</Typography.Title>
+        {renderAsync(asyncProject, {
+          loading: () => <Spinner />,
+          error: (error) => <p>{`Failed to load project: ${error.message}`}</p>,
+          data: (project) => {
+            if (!project) {
+              return <></>;
+            }
+            return (
+              <div className={styles.deploymentInfo}>
+                <DeploymentInfo deploymentId={project.deploymentId} project={project.metadata} />{' '}
+              </div>
+            );
+          },
+        })}
+      </div>
+    );
+  };
 
   return (
     <div>
@@ -63,7 +94,13 @@ export const SelectDeployment: React.VFC = () => {
       <Description />
       <div className={styles.searchDeployment}>
         <SearchAddress />
-        <StepButtons totalSteps={totalSteps} curStep={curStep} onStepChange={onStepChange} />
+        {searchedDeployment?.projectId && <DeploymentProject projectId={searchedDeployment?.projectId} />}
+        <StepButtons
+          totalSteps={totalSteps}
+          curStep={curStep}
+          onStepChange={onNext}
+          disabled={!searchedDeployment?.projectId}
+        />
       </div>
     </div>
   );
