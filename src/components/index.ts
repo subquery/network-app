2ca671045a8ed1b63adcdfc4433938d5b5cb82c5
Tane <82999197+tane-git@users.noreply.github.com>
2022-07-05 14:24:22 +1200

    Feature/add banner (#126)
    * add banner
    
    * clean up
    
    * feat: globalBanner improvment
    
    Co-authored-by: CaiYiLiang <cherryliang0112@gmail.com>

--- src/components/index.ts
+++ src/components/index.ts
@@ -43,6 +43,7 @@ export * from './SearchInput';
 export * from './GraphQLPlayground';
 export * from './Typography';
 export * from './Table';
+export * from './GlobalBanner';
 
 export {
   Header,
