ef22ad7943b4404973ee7dd6fbbae3d668bce5bc
CaiYiLiang <cherryliang0112@gmail.com>
2022-08-25 14:25:33 +1200

    feat: add swapExchange graphql client

--- src/containers/QueryApolloProvider.tsx
+++ src/containers/QueryApolloProvider.tsx
@@ -5,29 +5,25 @@ import { ApolloClient, ApolloLink, ApolloProvider, HttpLink, InMemoryCache } fro
 import { offsetLimitPagination } from '@apollo/client/utilities';
 import React from 'react';
 
+const getHttpLink = (uri: string | undefined) => new HttpLink({ uri });
+
 export const LEADERBOARD_CLIENT = 'leaderboard';
 export const SEASON_2_CLIENT = 'season2';
 export const SEASON_3_CLIENT = 'season3';
-const leaderboardLink = new HttpLink({
-  uri: process.env.REACT_APP_LEADERBOARD_PROJECT,
-});
+// const leaderboardLink = getHttpLink(process.env.REACT_APP_LEADERBOARD_PROJECT);
+// const season2Link = getHttpLink(process.env.REACT_APP_SEASON_2);
+// const season3Link = getHttpLink(process.env.REACT_APP_SEASON_3);
 
-const season2Link = new HttpLink({
-  uri: process.env.REACT_APP_SEASON_2,
-});
-
-const season3Link = new HttpLink({
-  uri: process.env.REACT_APP_SEASON_3,
-});
-const registryLink = new HttpLink({
-  uri: process.env.REACT_APP_QUERY_REGISTRY_PROJECT,
-});
+export const SWAP_EXCHANGE_CLIENT = 'season3';
+export const swapLink = getHttpLink(process.env.REACT_APP_QUERY_SWAP_EXCHANGE_PROJECT);
+const registryLink = getHttpLink(process.env.REACT_APP_QUERY_REGISTRY_PROJECT);
 
 export const QueryApolloProvider: React.FC = (props) => {
   const client = new ApolloClient({
     link: ApolloLink.split(
-      (operation) => operation.getContext().clientName === LEADERBOARD_CLIENT,
-      registryLink, // will use this link if client name specified in useQuery
+      (operation) => operation.getContext().clientName === SWAP_EXCHANGE_CLIENT,
+      swapLink,
+      registryLink,
     ),
     cache: new InMemoryCache({
       typePolicies: {
