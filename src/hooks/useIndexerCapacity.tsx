f0a3233789525505c4cc2cdc1ec6f540930f4126
Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>
2022-09-13 15:25:47 +1200

    fix indexer capacity calculation

--- src/hooks/useIndexerCapacity.tsx
+++ src/hooks/useIndexerCapacity.tsx
@@ -3,23 +3,27 @@
 
 import { BigNumber } from '@ethersproject/bignumber';
 import { useAsyncMemo, useEraValue } from '.';
-import { useContracts, useDelegation } from '../containers';
+import { useContracts, useDelegation, useIndexer } from '../containers';
 import { AsyncData } from '../utils';
 import { CurrentEraValue } from './useEraValue';
 
-export function useIndexerCapacity(indexer: string): AsyncData<CurrentEraValue<BigNumber> | undefined> {
+export function useIndexerCapacity(address: string): AsyncData<CurrentEraValue<BigNumber> | undefined> {
   const pendingContracts = useContracts();
-  const delegation = useDelegation(indexer, indexer);
+  const delegation = useDelegation(address, address);
   const indexerStake = useEraValue(delegation.data?.delegation?.amount);
+  const indexer = useIndexer({ address: address });
+  const indexerTotalStake = useEraValue(indexer?.data?.indexer?.totalStake);
 
   return useAsyncMemo(async () => {
     const contracts = await pendingContracts;
-
     if (!contracts) return;
 
     const leverageLimit = await contracts.staking.indexerLeverageLimit();
-    const current = indexerStake?.current.mul(leverageLimit) || BigNumber.from(0);
-    const after = (indexerStake?.after && indexerStake?.after.mul(leverageLimit)) || BigNumber.from(0);
+
+    const current = indexerStake?.current.mul(leverageLimit).sub(indexerTotalStake?.current || 0) || BigNumber.from(0);
+    const after =
+      (indexerStake?.after && indexerStake?.after.mul(leverageLimit).sub(indexerTotalStake?.after || 0)) ||
+      BigNumber.from(0);
 
     return { current, after };
   }, [indexerStake, pendingContracts]);
