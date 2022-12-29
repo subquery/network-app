ef09bc1e4a90aa39989c2141d2f59255f46a9dfa
CherryLiang <cherryliang0112@gmail.com>
2022-12-29 15:13:14 +1300

    feat:add zustand skeleton (#298)

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -6,3 +6,5 @@ export * from './studio';
 export * from './staking';
 export * from './plans';
 export * from './swap';
+
+export * from './delegator';
---
+++ src/utils/routes.ts
@@ -0,0 +1,18 @@
+// Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+ * Routes Example:
+ * - Explorer Root ->  EXPLORER: '/explorer',
+ * - Projects within Explorer -> PROJECTS_EXPLORER: '/explorer/projects',
+ *
+ */
+export const ROUTES = {
+  EXPLORER: '/explorer',
+
+  STUDIO: '/studio',
+
+  DELEGATOR: '/delegator',
+  INDEXER: '/indexer',
+  ALL_INDEXER: '/staking',
+};
