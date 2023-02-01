c2febbf7b213970cd6f897f913c33da7da292083
CherryLiang <cherryliang0112@gmail.com>
2023-02-01 14:08:35 +1300

    feat:group routes by tab/functionality

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,48 +1,69 @@
 // Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-export const ROUTES = {
+/**
+ * The routes should be grouped based on tabs
+ * Each routes group should come with ROOT ROUTE & SUB ROUTES
+ */
+
+// NOTE: Delegator tab
+const DELEGATOR = {
+  DELEGATOR: '/delegation',
+  DELEGATING: 'delegating',
+  INDEXERS: 'indexers',
+  ALL_INDEXERS: 'all',
+  TOP_INDEXERS: 'top',
+};
+
+// NOTE: Consumer tab
+const CONSUMER = {
+  CONSUMER: '/consumer',
+  FLEX_PLANS: 'flex-plans',
+  PLAYGROUND: 'playground',
+  ONGOING_PLANS: 'ongoing',
+  EXPIRED_PLANS: 'closed',
+  MY_OFFERS: 'my-offers',
+  OPEN_OFFERS: 'open',
+  CLOSE_OFFERS: 'close',
+  EXPIRED_OFFERS: 'expired',
+  CREATE_OFFER: 'create',
+  OFFER_MARKETPLACE: 'offers',
+};
+
+// TODO: Remove or ReOrg the “old” paths once renovation done
+const PATHS = {
+  ...DELEGATOR,
+  ...CONSUMER,
   // ROOT PATHS
   EXPLORER: '/explorer',
   STUDIO: '/studio',
   SWAP: '/swap',
   STAKING: '/staking', //todo: improve
   PLANS: '/plans',
-  DELEGATOR: '/delegation',
-  CONSUMER: '/consumer',
 
   // RELATIVE PATHS
   OVERVIEW: 'overview',
   INDEXER: 'indexer',
   INDEXERS: 'indexers',
   INDEXING: 'indexing',
-  DELEGATING: 'delegating',
   DELEGATE: 'delegate',
-  FLEX_PLANS: 'flex-plans',
   MY_PROFILE: 'my-profile',
   MY_PLANS: 'my-plans',
-  MY_OFFERS: 'my-offers',
-  OFFER_MARKETPLACE: 'offers',
-  OPEN_OFFERS: 'open',
-  CLOSE_OFFERS: 'close',
-  EXPIRED_OFFERS: 'expired',
-  CREATE_OFFER: 'create',
   DEFAULT_PLANS: 'default',
   SPECIFIC_PLANS: 'specific',
   PLAYGROUND: 'playground',
-  ONGOING_PLANS: 'ongoing',
-  EXPIRED_PLANS: 'closed',
   SERVICE_AGREEMENTS: 'service-agreements',
-  ALL_INDEXERS: 'all',
-  TOP_INDEXERS: 'top',
   REWARDS: 'rewards',
   LOCKED: 'locked',
   DETAILS: 'details',
   DEPLOYMENTS: 'deployments',
   SELL: 'sell',
   BUY: 'buy',
+};
 
+const NAV_LINKS = {
   // NAVIGATION LINKS
+  // TODO: Remove or ReOrg the “old” paths once renovation done
   OPEN_OFFERS_NAV: '/plans/my-offers/open',
   EXPIRED_OFFERS_NAV: '/plans/my-offers/expired',
   OFFER_MARKETPLACE_NAV: '/plans/offers',
@@ -54,11 +75,16 @@ export const ROUTES = {
   STUDIO_PROJECT_NAV: '/studio/project',
   STUDIO_CREATE_NAV: '/studio/create',
 
-  PLAYGROUND_NAV: '/consumer/flex-plans/playground',
-  CLOSED_PLANS_NAV: '/consumer/flex-plans/closed',
-  ONGOING_PLANS_NAV: '/consumer/flex-plans/ongoing',
-  CONSUMER_OFFERS_NAV: '/consumer/my-offers',
-  CONSUMER_OPEN_OFFERS_NAV: '/consumer/my-offers/open',
-  CONSUMER_EXPIRED_OFFERS_NAV: '/consumer/my-offers/expired',
-  CONSUMER_OFFER_MARKETPLACE_NAV: '/consumer/offers',
+  PLAYGROUND_NAV: `${CONSUMER.CONSUMER}/${CONSUMER.FLEX_PLANS}/${CONSUMER.PLAYGROUND}`,
+  CLOSED_PLANS_NAV: `${CONSUMER.CONSUMER}/${CONSUMER.FLEX_PLANS}/${CONSUMER.EXPIRED_PLANS}`,
+  ONGOING_PLANS_NAV: `${CONSUMER.CONSUMER}/${CONSUMER.FLEX_PLANS}/${CONSUMER.ONGOING_PLANS}`,
+  CONSUMER_OFFERS_NAV: `${CONSUMER.CONSUMER}/${CONSUMER.MY_OFFERS}`,
+  CONSUMER_OPEN_OFFERS_NAV: `${CONSUMER.CONSUMER}/${CONSUMER.MY_OFFERS}/${CONSUMER.OPEN_OFFERS}`,
+  CONSUMER_EXPIRED_OFFERS_NAV: `${CONSUMER.CONSUMER}/${CONSUMER.MY_OFFERS}/${CONSUMER.EXPIRED_OFFERS}`,
+  CONSUMER_OFFER_MARKETPLACE_NAV: `${CONSUMER.CONSUMER}/${CONSUMER.OFFER_MARKETPLACE}`,
+};
+
+export const ROUTES = {
+  ...PATHS,
+  ...NAV_LINKS,
 };
