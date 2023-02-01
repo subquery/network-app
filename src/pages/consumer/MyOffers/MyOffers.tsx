fc27360dca5b296347b9c67f3f58ab5fbddb7843
Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>
2023-02-02 11:29:26 +1300

    fix my offers page

--- src/pages/consumer/MyOffers/MyOffers.tsx
+++ src/pages/consumer/MyOffers/MyOffers.tsx
@@ -109,10 +109,15 @@ const MyOffer: React.FC<MyOfferProps> = ({ queryFn, totalCount, description }) =
 
   return (
     <>
-      <OfferHeader />
-      <div className="contentContainer">
-        <OfferTable queryFn={queryFn} queryParams={{ consumer: account || '' }} description={description} />
-      </div>
+      {totalCount <= 0 && <Navigate replace to={CONSUMER_OFFERS_NAV} />}
+      {totalCount > 0 && (
+        <>
+          <OfferHeader />
+          <div className="contentContainer">
+            <OfferTable queryFn={queryFn} queryParams={{ consumer: account || '' }} description={description} />
+          </div>
+        </>
+      )}
     </>
   );
 };
