a175e40439b86266073539ac54821d161dcfda01
Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>
2023-02-09 13:35:16 +1300

    set dev package version, add intl, add routes

--- src/pages/indexer/index.tsx
+++ src/pages/indexer/index.tsx
@@ -11,7 +11,7 @@ import { ROUTES } from '../../utils';
 import { Marketplace } from './OfferMarketplace';
 import ServiceAgreements from '../consumer/ServiceAgreements';
 
-const { MY_STAKING, SERVICE_AGREEMENTS, OFFER_MARKETPLACE } = ROUTES;
+const { MY_STAKING, SERVICE_AGREEMENTS, OFFER_MARKETPLACE, MY_DELEGATORS } = ROUTES;
 
 export const Indexer: React.VFC = () => {
   const { t } = useTranslation();
@@ -26,6 +26,10 @@ export const Indexer: React.VFC = () => {
       link: SERVICE_AGREEMENTS,
     },
     {
+      label: t('indexer.myDelegators'),
+      link: MY_DELEGATORS,
+    },
+    {
       label: t('plans.category.offerMarketplace'),
       link: OFFER_MARKETPLACE,
     },
@@ -35,6 +39,7 @@ export const Indexer: React.VFC = () => {
     <AppSidebar list={sidebarList}>
       <Routes>
         <Route path={`${MY_STAKING}/*`} element={<MyStaking />} />
+        <Route path={`${MY_DELEGATORS}/*`} element={<MyDelegators />} />
         <Route path={`${OFFER_MARKETPLACE}/*`} element={<Marketplace />} />
         <Route path={`${SERVICE_AGREEMENTS}/*`} element={<ServiceAgreements USER_ROLE={'indexer'} />} />
         <Route path={`${OFFER_MARKETPLACE}/*`} element={<Marketplace />} />
