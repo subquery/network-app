98013eb87237366c8daaeb606623dc28f0a58550
CherryLiang <cherryliang0112@gmail.com>
2023-02-21 14:27:08 +1300

    feat: remove unused code

--- src/pages/indexer/MyStaking/MyStaking.tsx
+++ src/pages/indexer/MyStaking/MyStaking.tsx
@@ -42,8 +42,6 @@ export const MyStaking: React.VFC = () => {
 
             if (indexer === undefined && !s) return <Spinner />;
             if (!indexer && !s) return <NotRegisteredIndexer />;
-            console.log('s', s);
-            console.log('indexer', indexer);
 
             const sortedTotalStaking = truncFormatEtherStr(`${s?.totalStake.current ?? 0}`);
 
