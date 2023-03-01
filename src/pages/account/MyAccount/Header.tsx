8515fd03a53ff4f64ac566dfdcec270467cd66d2
Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>
2023-03-01 18:14:49 +1300

    fix git history

---
+++ src/pages/account/MyAccount/Header.tsx
@@ -0,0 +1,35 @@
+// Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { Copy, CurEra } from '@components';
+import { useWeb3 } from '@containers';
+import { truncateAddress } from '@subql/react-ui/dist/components/Address/Address';
+import { Typography } from '@subql/components';
+import styles from './MyAccount.module.css';
+import ReactJazzicon from 'react-jazzicon';
+import { Link } from 'react-router-dom';
+
+export const AccountHeader: React.FC = () => {
+  const { account } = useWeb3();
+
+  return (
+    <div className={styles.header}>
+      <div className={styles.accountContainer}>
+        <div className={styles.icon}>
+          <ReactJazzicon diameter={70} />
+        </div>
+        <div className={styles.account}>
+          <Copy value={account ?? ''} iconClassName={styles.copyIcon}>
+            <Typography variant="h5" weight={900}>
+              {truncateAddress(account ?? '')}
+            </Typography>
+          </Copy>
+          {/* <Typography variant="medium" className={styles.link}>
+            <Link to={'/'}>{'Manage Controller Account'}</Link>
+          </Typography> */}
+        </div>
+      </div>
+      <CurEra />
+    </div>
+  );
+};
---
+++ src/pages/account/index.tsx
@@ -0,0 +1,21 @@
+// Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { useWeb3 } from '@containers';
+import * as React from 'react';
+import { Route, Routes } from 'react-router';
+import { ROUTES } from '@utils';
+import { MyAccount } from './MyAccount';
+import { Rewards } from './Rewards/Rewards';
+
+const { REWARDS } = ROUTES;
+
+export const Account: React.FC = () => {
+  const { account } = useWeb3();
+  return (
+    <Routes>
+      <Route path={REWARDS} element={<Rewards delegator={account ?? ''} />} />
+      <Route path={'/'} element={<MyAccount />} />
+    </Routes>
+  );
+};
