e9ca91bafe4c9e372dfc17bb6f2ff6563be2719c
Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>
2023-03-03 15:08:53 +1300

    re-add rewards code

--- src/pages/account/Rewards/Rewards.tsx
+++ src/pages/account/Rewards/Rewards.tsx
@@ -3,26 +3,40 @@
 
 import { Spinner, Typography } from '@subql/react-ui';
 import * as React from 'react';
-import { Table, TableProps, Tag } from 'antd';
+import { Breadcrumb, Table, TableProps, Tag } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { useWeb3 } from '@containers';
-import { formatEther, mapAsync, notEmpty, renderAsyncArray } from '@utils';
+import { formatEther, mapAsync, notEmpty, renderAsyncArray, ROUTES } from '@utils';
 import { RewardFieldsFragment as Reward, UnclaimedRewardFieldsFragment as UnclaimedReward } from '@subql/network-query'
 import ClaimRewards from './ClaimRewards';
 import styles from './Rewards.module.css';
-import { TableText } from '@components';
+import { AppPageHeader, TableText } from '../../../components';
 import { BigNumber } from 'ethers';
-import { TokenAmount } from '@components/TokenAmount';
+import { TokenAmount } from '../../../components/TokenAmount';
 import { useGetRewardsQuery } from '@subql/react-hooks';
+import { TableTitle } from '@subql/components';
 
 function isClaimedReward(reward: Reward | UnclaimedReward): reward is Reward {
   return !!(reward as Reward).claimedTime;
 }
 
+// TODO: Confirm with design team for component level
+const RewardsSubRoutes = () => {
+  const { t } = useTranslation('translation');
+  return (
+    <Breadcrumb separator=">">
+      <Breadcrumb.Item href={ROUTES.MY_ACCOUNT_NAV} className={styles.title}>
+        {t('indexer.indexers')}
+      </Breadcrumb.Item>
+      <Breadcrumb.Item className={styles.title}>{t('rewards.claim.title')}</Breadcrumb.Item>
+    </Breadcrumb>
+  );
+};
+
 export const Rewards: React.FC<{ delegator: string }> = ({ delegator }) => {
   const { account } = useWeb3();
   const rewards = useGetRewardsQuery({ variables: { address: delegator } });
-  const { t } = useTranslation('translation');
+  const { t } = useTranslation();
 
   const columns: TableProps<Reward | UnclaimedReward>['columns'] = [
     {
@@ -31,19 +45,19 @@ export const Rewards: React.FC<{ delegator: string }> = ({ delegator }) => {
       render: (t, r, index) => <TableText content={index + 1} />,
     },
     {
-      title: t('rewards.indexer').toUpperCase(),
+      title: <TableTitle title={t('rewards.indexer')} />,
       dataIndex: 'indexerAddress',
       key: 'indexer',
       render: (text: string) => <TableText content={text} />,
     },
     {
-      title: t('rewards.amount').toUpperCase(),
+      title: <TableTitle title={t('rewards.amount')} />,
       dataIndex: 'amount',
       key: 'amount',
       render: (amount: bigint) => <TokenAmount value={formatEther(amount)} />,
     },
     {
-      title: t('rewards.action').toUpperCase(),
+      title: <TableTitle title={t('rewards.action')} />,
       dataIndex: 'action',
       key: 'action',
       render: (_, reward: Reward | UnclaimedReward) => {
@@ -55,52 +69,57 @@ export const Rewards: React.FC<{ delegator: string }> = ({ delegator }) => {
   ];
 
   return (
-    <div className={'contentContainer'}>
-      {renderAsyncArray(
-        mapAsync(
-          (data) =>
-            ((data.unclaimedRewards?.nodes.filter(notEmpty) as Array<UnclaimedReward | Reward>) ?? []).concat(
-              data.rewards?.nodes.filter(notEmpty) ?? [],
-            ),
-          rewards,
-        ),
-        {
-          error: (error) => <Typography>{`Failed to get pending rewards: ${error.message}`}</Typography>,
-          loading: () => <Spinner />,
-          empty: () => <Typography variant="h6">{t('rewards.none')}</Typography>,
-          data: (data) => {
-            const totalUnclaimedRewards = rewards?.data?.unclaimedRewards?.totalCount || 0;
-            const unclaimedRewards = rewards?.data?.unclaimedRewards?.nodes?.reduce(
-              (result, unclaimedReward) => {
-                const totalUnclaimed = result.totalAmount.add(BigNumber.from(unclaimedReward?.amount ?? '0'));
-                const sortedIndexers = [...result.indexers, unclaimedReward?.indexerAddress];
-                return { indexers: sortedIndexers, totalAmount: totalUnclaimed };
-              },
-              {
-                indexers: [] as Array<string | undefined>,
-                totalAmount: BigNumber.from('0'),
-              },
-            );
-            return (
-              <>
-                <div className="flex-between">
-                  <Typography variant="h6" className={styles.header}>
-                    {t('rewards.totalUnclaimReward', { count: totalUnclaimedRewards })}
-                  </Typography>
-                  {totalUnclaimedRewards > 0 && unclaimedRewards?.indexers && (
-                    <ClaimRewards
-                      indexers={unclaimedRewards?.indexers as string[]}
-                      account={account ?? ''}
-                      totalUnclaimed={formatEther(unclaimedRewards?.totalAmount)}
-                    />
-                  )}
-                </div>
-                <Table columns={columns} dataSource={data} scroll={{ x: 600 }} rowKey="id" />
-              </>
-            );
+    <div className={styles.rewardsContainer}>
+      <RewardsSubRoutes />
+      <AppPageHeader title={t('rewards.claim.title')} desc={t('rewards.description')} />
+
+      <div className={styles.rewardsList}>
+        {renderAsyncArray(
+          mapAsync(
+            (data) =>
+              ((data.unclaimedRewards?.nodes.filter(notEmpty) as Array<UnclaimedReward | Reward>) ?? []).concat(
+                (data.rewards?.nodes.filter(notEmpty) as Array<Reward>) ?? [],
+              ),
+            rewards,
+          ),
+          {
+            error: (error) => <Typography>{`Failed to get pending rewards: ${error.message}`}</Typography>,
+            loading: () => <Spinner />,
+            empty: () => <Typography variant="h6">{t('rewards.none')}</Typography>,
+            data: (data) => {
+              const totalUnclaimedRewards = rewards?.data?.unclaimedRewards?.totalCount || 0;
+              const unclaimedRewards = rewards?.data?.unclaimedRewards?.nodes?.reduce(
+                (result, unclaimedReward) => {
+                  const totalUnclaimed = result.totalAmount.add(BigNumber.from(unclaimedReward?.amount ?? '0'));
+                  const sortedIndexers = [...result.indexers, unclaimedReward?.indexerAddress];
+                  return { indexers: sortedIndexers, totalAmount: totalUnclaimed };
+                },
+                {
+                  indexers: [] as Array<string | undefined>,
+                  totalAmount: BigNumber.from('0'),
+                },
+              );
+              return (
+                <>
+                  <div className="flex-between">
+                    <Typography variant="h6" className={styles.header}>
+                      {t('rewards.totalUnclaimReward', { count: totalUnclaimedRewards })}
+                    </Typography>
+                    {totalUnclaimedRewards > 0 && unclaimedRewards?.indexers && (
+                      <ClaimRewards
+                        indexers={unclaimedRewards?.indexers as string[]}
+                        account={account ?? ''}
+                        totalUnclaimed={formatEther(unclaimedRewards?.totalAmount)}
+                      />
+                    )}
+                  </div>
+                  <Table columns={columns} dataSource={data} scroll={{ x: 600 }} rowKey="id" />
+                </>
+              );
+            },
           },
-        },
-      )}
+        )}
+      </div>
     </div>
   );
 };
