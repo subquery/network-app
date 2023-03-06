298094ac9a60ba0e60e7f9d66b64e85bbbe4e0b4
HZHAIX <zhaihaohk@gmail.com>
2023-03-07 11:52:14 +1300

    Use stable token address from env (#349)

--- src/utils/USDC/index.ts
+++ src/utils/USDC/index.ts
@@ -4,9 +4,8 @@
 import { ethers, Contract } from 'ethers';
 import abi from './USDC.json';
 
-// NOTE: Tmp solution and will replace with aUSD contract sdk
 // NOTE: Why not use hook: plan to move to network-client
-export const STABLE_TOKEN_ADDRESS = '0x24BCD6845616f72803681e2288547F3922a1C8f6';
+export const STABLE_TOKEN_ADDRESS = import.meta.env.VITE_STABLE_TOKEN_ADDRESS;
 export const STABLE_TOKEN_DECIMAL = 6;
 export const initialAUSDContract = async (): Promise<Contract> => {
   const ethereum = (window as any).ethereum;
