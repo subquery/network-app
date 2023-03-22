7ec342aefc6b2140fe478cce0d8eaacf0cc782f0
CherryLiang <cherryliang0112@gmail.com>
2023-03-23 11:28:47 +1300

    feat/package conflict (#379)
    * feat: solve antd version conflict
    
    * feat: layout & UI

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -15,7 +15,7 @@ type Props = {
   }[];
 };
 
-export const Sidebar: React.VFC<Props> = ({ list }) => {
+export const Sidebar: React.FC<Props> = ({ list }) => {
   return (
     <div className={styles.sidebar}>
       {list.map((sidebarItem) => (
