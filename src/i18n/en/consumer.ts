e57e583d5b80ea8291843db1a4a511aeb3ba3b36
HZ <zhaihaohk@gmail.com>
2023-05-31 12:35:08 +1200

    Update USDC address on Testnet

--- src/i18n/en/consumer.ts
+++ src/i18n/en/consumer.ts
@@ -326,8 +326,8 @@ const translation = {
     },
   },
   swap: {
-    buyKSQT: `${STABLE_TOKEN} → ${TOKEN}`,
-    sellKSQT: `${TOKEN} → ${STABLE_TOKEN}`,
+    buykSQT: `${STABLE_TOKEN} → ${TOKEN}`,
+    sellkSQT: `${TOKEN} → ${STABLE_TOKEN}`,
     poolSize: `${TOKEN} pool size`,
     nonOrder: `There is no available order in pool currently. `,
     nonOrderDesc: `Currently, there are no active swap orders available. However, don't worry - when orders become available, you can convert between USDC and kSQT using our basic swap mechanism. Get ready to swap with Kepler!`,
