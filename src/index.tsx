bae47c123a6a504e8d1a5bf3d73aa6240dcf1606
cyrbuzz <cyrbuzz@foxmail.com>
2023-06-19 17:51:16 +0800

    chore: arrange env

--- src/index.tsx
+++ src/index.tsx
@@ -19,7 +19,7 @@ const tagManagerArgs = {
   gtmId: 'G-DK4PX8F61X',
 };
 
-const isProd = import.meta.env.NODE_ENV === 'production';
+const isProd = import.meta.env.PROD;
 isProd && TagManager.initialize(tagManagerArgs);
 
 ReactDOM.render(<App />, document.getElementById('root'));
