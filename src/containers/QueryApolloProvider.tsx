a45f08598ee2b00ee229f130b4f4797f3f013b83
cyrbuzz <cyrbuzz@foxmail.com>
2023-06-27 17:58:17 +0800

    feat: make registry decentralise

--- src/containers/QueryApolloProvider.tsx
+++ src/containers/QueryApolloProvider.tsx
@@ -13,24 +13,32 @@ export const SWAP_EXCHANGE_CLIENT = 'swapExchange';
 export const TOP_100_INDEXERS = 'top100Indexers';
 const top100IndexersLink = getHttpLink(import.meta.env.VITE_TOP_100_INDEXERS);
 
-const registryLink = getHttpLink(import.meta.env.VITE_QUERY_REGISTRY_PROJECT);
-
-const getSwapLink = () => {
+const getDecentraliseLink = (deploymentId: string, fallbackServiceUrl?: string) => {
   const httpOptions = { fetch, fetchOptions: { timeout: 5000 } };
 
   return deploymentHttpLink({
-    deploymentId: import.meta.env.VITE_SUBQUERY_DEPLOYMENT_ID,
+    deploymentId,
     httpOptions,
     authUrl: import.meta.env.VITE_AUTH_URL,
-    fallbackServiceUrl: import.meta.env.VITE_QUERY_SWAP_EXCHANGE_PROJECT,
+    fallbackServiceUrl,
   });
 };
+
+const swapLink = getDecentraliseLink(
+  import.meta.env.VITE_SUBQUERY_DEPLOYMENT_ID,
+  import.meta.env.VITE_QUERY_SWAP_EXCHANGE_PROJECT,
+);
+
+const registryLink = getDecentraliseLink(
+  import.meta.env.VITE_QUERY_REGISTRY_DEPLOYMENT_ID,
+  import.meta.env.VITE_QUERY_REGISTRY_PROJECT,
+);
 
 export const QueryApolloProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const client = new ApolloClient({
     link: ApolloLink.split(
       (operation) => operation.getContext().clientName === SWAP_EXCHANGE_CLIENT,
-      getSwapLink(),
+      swapLink,
       ApolloLink.split(
         (operation) => operation.getContext().clientName === TOP_100_INDEXERS,
         top100IndexersLink,
