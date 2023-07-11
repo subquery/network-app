bbc6fdbb5f4b39a26e0f33695fda6bf69757c3ec
Cyrbuzz <cyrbuzz@foxmail.com>
2023-07-11 15:55:00 +0800

    feat: Catch graphql error to Sentry. (#476)
    * feat: gather console.error msg
    
    * feat: catch graphql error

--- src/containers/QueryApolloProvider.tsx
+++ src/containers/QueryApolloProvider.tsx
@@ -3,7 +3,9 @@
 
 import React, { PropsWithChildren } from 'react';
 import { ApolloClient, ApolloLink, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
 import { offsetLimitPagination } from '@apollo/client/utilities';
+import { captureException } from '@sentry/react';
 import { deploymentHttpLink } from '@subql/apollo-links';
 
 const getHttpLink = (uri: string | undefined) => new HttpLink({ uri });
@@ -38,17 +40,45 @@ const registryLink = getDecentraliseLink(
   import.meta.env.VITE_QUERY_REGISTRY_PROJECT,
 );
 
+const links = ApolloLink.from([
+  onError(({ graphQLErrors, operation }) => {
+    // Filter consumer by community.
+    // If community link goes error, apollo-links will try until all failed.
+    // So Just catch the error cause by fallback service.
+    try {
+      const {
+        response: { url },
+      } = operation.getContext();
+
+      if (url && url.match(/.+\/query\/[0-9a-zA-Z]{46}/)) return;
+    } finally {
+      // don't care there have errors.
+    }
+
+    captureException(`Query fetch`, {
+      extra: {
+        graphqlError: graphQLErrors?.reduce((a, b) => ({ message: a.message + ' ' + b.message }), { message: '' })
+          .message,
+        operation: operation.query.loc?.source.body,
+      },
+    });
+  }),
+  ApolloLink.split(
+    (operation) => operation.getContext().clientName === SWAP_EXCHANGE_CLIENT,
+    swapLink,
+
+    ApolloLink.split(
+      (operation) => operation.getContext().clientName === TOP_100_INDEXERS,
+      top100IndexersLink,
+
+      registryLink,
+    ),
+  ),
+]);
+
 export const QueryApolloProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const client = new ApolloClient({
-    link: ApolloLink.split(
-      (operation) => operation.getContext().clientName === SWAP_EXCHANGE_CLIENT,
-      swapLink,
-      ApolloLink.split(
-        (operation) => operation.getContext().clientName === TOP_100_INDEXERS,
-        top100IndexersLink,
-        registryLink,
-      ),
-    ),
+    link: links,
     cache: new InMemoryCache({
       typePolicies: {
         Query: {
