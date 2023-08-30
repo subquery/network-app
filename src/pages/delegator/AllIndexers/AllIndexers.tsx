62df909f06979b1ac7113d16a1dd8eda3efcb2cd
Cyrbuzz <cyrbuzz@foxmail.com>
2023-08-30 18:38:06 +0800

    fix: no login all indexer pages (#537)

--- src/pages/delegator/AllIndexers/AllIndexers.tsx
+++ src/pages/delegator/AllIndexers/AllIndexers.tsx
@@ -3,7 +3,7 @@
 
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
-import { EmptyList } from '@components';
+import { EmptyList, WalletRoute } from '@components';
 import { useEra } from '@hooks';
 import { Spinner, Typography } from '@subql/components';
 import { useGetIndexersQuery } from '@subql/react-hooks';
@@ -20,31 +20,41 @@ export const AllIndexers: React.FC = () => {
   };
 
   return (
-    <div>
-      {renderAsync(
-        mapAsync(
-          ([data, curEra]) => ({
-            data: data?.indexers?.nodes.filter(notEmpty),
-            totalCount: data?.indexers?.totalCount,
-            era: curEra?.index,
-          }),
-          mergeAsync(indexers, currentEra),
-        ),
-        {
-          loading: () => <Spinner />,
-          error: (error) => <Typography>{`Error: Failed to get Indexers: ${error.message}`}</Typography>,
-          data: (data) => {
-            if (!data || data?.totalCount === 0) {
-              <EmptyList title={t('allIndexers.nonData')} description={t('allIndexers.desc')}></EmptyList>;
-            }
+    <WalletRoute
+      componentMode
+      element={
+        <div>
+          {renderAsync(
+            mapAsync(
+              ([data, curEra]) => ({
+                data: data?.indexers?.nodes.filter(notEmpty),
+                totalCount: data?.indexers?.totalCount,
+                era: curEra?.index,
+              }),
+              mergeAsync(indexers, currentEra),
+            ),
+            {
+              loading: () => <Spinner />,
+              error: (error) => <Typography>{`Error: Failed to get Indexers: ${error.message}`}</Typography>,
+              data: (data) => {
+                if (!data || data?.totalCount === 0) {
+                  <EmptyList title={t('allIndexers.nonData')} description={t('allIndexers.desc')}></EmptyList>;
+                }
 
-            return (
-              <IndexerList indexers={data.data} totalCount={data.totalCount} onLoadMore={fetchMore} era={data.era} />
-            );
-          },
-        },
-      )}
-    </div>
+                return (
+                  <IndexerList
+                    indexers={data.data}
+                    totalCount={data.totalCount}
+                    onLoadMore={fetchMore}
+                    era={data.era}
+                  />
+                );
+              },
+            },
+          )}
+        </div>
+      }
+    ></WalletRoute>
   );
 };
 
