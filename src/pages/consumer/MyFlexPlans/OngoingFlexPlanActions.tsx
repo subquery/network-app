add5bcea08d3d5376079ec20d0b0350027e55884
cyrbuzz <cyrbuzz@foxmail.com>
2023-10-13 15:27:03 +0800

    feat: terminate by consumer

--- src/pages/consumer/MyFlexPlans/OngoingFlexPlanActions.tsx
+++ src/pages/consumer/MyFlexPlans/OngoingFlexPlanActions.tsx
@@ -4,50 +4,21 @@
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router';
-import { Web3Provider } from '@ethersproject/providers';
 import { useConsumerHostServices } from '@hooks/useConsumerHostServices';
 import { StateChannelFieldsFragment as ConsumerFlexPlan } from '@subql/network-query';
 import { Button } from 'antd';
 import { BigNumber } from 'ethers';
 
+import { useWeb3Store } from 'src/stores';
+
 import { AppTypography, SummaryList } from '../../../components';
 import TransactionModal from '../../../components/TransactionModal';
-import { useWeb3 } from '../../../containers';
-import { getAuthReqHeader, parseError, TOKEN } from '../../../utils';
+import { TOKEN } from '../../../utils';
 import { ROUTES } from '../../../utils';
-import { requestConsumerHostToken } from '../../../utils/eip721SignTokenReq';
 import { formatEther } from '../../../utils/numberFormatters';
 import styles from './MyFlexPlans.module.css';
 
 const { PLAYGROUND_NAV } = ROUTES;
-
-// async function terminatePlan(flexPlanId: string, account: string, library: Web3Provider | undefined) {
-//   try {
-//     const { error, data: consumerToken } = await requestConsumerHostToken(account, library);
-
-//     if (error || !consumerToken) {
-//       throw new Error('Failed to request user authentication.');
-//     }
-
-//     const terminateUrl = `${import.meta.env.VITE_CONSUMER_HOST_ENDPOINT}/users/channels/${flexPlanId}/finalize`;
-
-//     const response = await fetch(terminateUrl, {
-//       headers: { ...getAuthReqHeader(consumerToken) },
-//       method: 'POST',
-//     });
-
-//     const sortedResponse = response && (await response.json());
-
-//     if (!response?.ok || sortedResponse?.error) {
-//       throw new Error(sortedResponse?.error);
-//     }
-
-//     return { data: sortedResponse };
-//   } catch (error) {
-//     parseError(error);
-//     return { error };
-//   }
-// }
 
 interface IOngoingFlexPlanActions {
   flexPlan: ConsumerFlexPlan;
@@ -56,10 +27,18 @@ interface IOngoingFlexPlanActions {
 
 const useTerminatePlan = () => {
   const { getUserChannelState } = useConsumerHostServices({ autoLogin: false });
-
+  const { contracts } = useWeb3Store();
   const terminatePlan = async (flexPlanId: string) => {
     const res = await getUserChannelState(flexPlanId);
-    console.warn(res);
+
+    if (res.data.channelId) {
+      const hash = await contracts?.stateChannel.terminate(res.data);
+      const result = await hash?.wait();
+
+      return result?.status;
+    }
+
+    return false;
   };
   return {
     terminatePlan,
@@ -70,7 +49,6 @@ export const OngoingFlexPlanActions: React.FC<IOngoingFlexPlanActions> = ({ flex
   const { t } = useTranslation();
   const [isLoading, setIsLoading] = React.useState<boolean>();
   const [error, setError] = React.useState<string>();
-  const { account, library } = useWeb3();
   const { total, spent } = flexPlan;
   const remainDeposit = formatEther(BigNumber.from(total).sub(BigNumber.from(spent)), 4);
   const navigate = useNavigate();
@@ -86,9 +64,10 @@ export const OngoingFlexPlanActions: React.FC<IOngoingFlexPlanActions> = ({ flex
     setIsLoading(true);
     try {
       const terminateResult = await terminatePlan(flexPlan.id);
-
-      onSuccess && onSuccess();
-      onCancel();
+      if (terminateResult) {
+        onSuccess && onSuccess();
+        onCancel();
+      }
     } catch (e) {
       setError(`Failed to terminated. ${e}`);
     } finally {
