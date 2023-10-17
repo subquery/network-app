1dfc7285991875e2b9a7029c82b307653e8ab38c
cyrbuzz <cyrbuzz@foxmail.com>
2023-10-17 11:14:04 +0800

    feat: sorting

--- src/pages/explorer/Home/Home.tsx
+++ src/pages/explorer/Home/Home.tsx
@@ -6,10 +6,13 @@ import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router';
 import { SearchOutlined } from '@ant-design/icons';
 import { Typography } from '@subql/components';
-import { ProjectFieldsFragment as Project } from '@subql/network-query';
-import { useGetProjectsLazyQuery } from '@subql/react-hooks';
+import { ProjectFieldsFragment as Project, ProjectUpdateFieldFragment } from '@subql/network-query';
+import { useGetProjectsAllLazyQuery, useGetProjectsLazyQuery } from '@subql/react-hooks';
+import { makeCacheKey } from '@utils/limitation';
 import { useInfiniteScroll } from 'ahooks';
 import { Input } from 'antd';
+import BigNumber from 'bignumber.js';
+import localforage from 'localforage';
 
 import { ProjectCard, Spinner } from '../../../components';
 import { useProjectMetadata } from '../../../containers';
@@ -51,24 +54,110 @@ export const Header: React.FC = () => {
   );
 };
 
+const sortProjects = (projects: Project[]) => {
+  return projects
+    .map((proj) => ({
+      ...proj,
+      amount: proj.deployments.nodes.reduce((cur, add) => {
+        const lockedAmount = BigNumber(add?.serviceAgreements?.aggregates?.sum?.lockedAmount.toString() || '0');
+
+        return cur.plus(lockedAmount);
+      }, BigNumber('0')),
+    }))
+    .sort((a, b) => (a.amount.gt(b.amount) ? -1 : 1));
+};
+
+const useCachedProjects = () => {
+  const cacheKey = React.useRef(makeCacheKey('projects'));
+
+  const getCachedProjects: () => Promise<Project[]> = async () => {
+    try {
+      const result = await localforage.getItem<Project[]>(cacheKey.current);
+      return result || [];
+    } catch {
+      return [];
+    }
+  };
+
+  const setCachedProjectgs = async (
+    data: (ProjectUpdateFieldFragment | Project)[],
+    mode: 'merge' | 'update' = 'merge',
+  ) => {
+    try {
+      const old = await getCachedProjects();
+
+      // for merge mode, suppose the data is Project[]
+      if (mode === 'merge') {
+        return await localforage.setItem(cacheKey.current, sortProjects([...old, ...(data as Project[])]));
+      } else if (mode === 'update') {
+        const newData = old.map((item) => {
+          return {
+            ...item,
+            ...data.find((i) => i.id === item.id),
+          };
+        });
+        return await localforage.setItem(cacheKey.current, sortProjects(newData));
+      }
+    } catch (e) {
+      console.error('Set Cached failed');
+      console.error(e);
+    }
+  };
+
+  return {
+    getCachedProjects,
+    setCachedProjectgs,
+  };
+};
+
 const Home: React.FC = () => {
   const [getProjects, { error, loading }] = useGetProjectsLazyQuery({
     variables: { offset: 0 },
   });
+
+  const [getSpecFiledOfProjects, { error: updateError, loading: updateLoading }] = useGetProjectsAllLazyQuery();
+
   const navigate = useNavigate();
+  const times = React.useRef(0);
+  const { getCachedProjects, setCachedProjectgs } = useCachedProjects();
 
   const [projects, setProjects] = React.useState<Project[]>([]);
 
   const loadMore = async () => {
+    // there have a flow:
+    //   1. Load cache, when first time call this function
+    //   2. If there have cached data, then request a update request for exist data and update it then set to Projects.
+    //   3. When the second times or not have cached data, getProject with variable that not in cached data.
+    if (times.current === 0) {
+      let cachedProjects = await getCachedProjects();
+      times.current = 1;
+      if (cachedProjects.length) {
+        const updatedData = await getSpecFiledOfProjects();
+        cachedProjects =
+          (await setCachedProjectgs(updatedData.data?.projects?.nodes.filter(notEmpty) || [], 'update')) || [];
+      }
+      setProjects(cachedProjects);
+
+      return {
+        list: [],
+        isNoMore: false,
+      };
+    }
+
     const res = await getProjects({
       variables: {
-        offset: projects.length,
+        offset: 0,
+        ids: projects.map((i) => i.id),
       },
     });
     // implement a sorting for projects
     if (res.data?.projects?.nodes) {
-      // const sorted
-      setProjects([...projects, ...res.data.projects?.nodes.filter(notEmpty)]);
+      const nonEmptyProjects = res.data.projects?.nodes.filter(notEmpty);
+      // be care of this part, do not set cached data when non-first fetch.
+      // the flow will be break & pages will show flush and confuse to user.
+      await setCachedProjectgs(nonEmptyProjects);
+      const sorted = sortProjects(nonEmptyProjects);
+      setProjects([...projects, ...sorted]);
     }
 
     return {
@@ -86,9 +175,9 @@ const Home: React.FC = () => {
   return (
     <div className={styles.explorer}>
       <Header />
-      {error && <span>{`We have an error: ${error.message}`}</span>}
-
-      <div style={{ display: 'flex', marginBottom: 32 }}>
+      {(error || updateError) && <span>{`We have an error: ${updateError?.message || error?.message}`}</span>}
+      {/* TODO: finish this part */}
+      {/* <div style={{ display: 'flex', marginBottom: 32 }}>
         <span style={{ flex: 1 }}></span>
         <Input
           style={{ width: 200, height: 48, padding: 12 }}
@@ -100,7 +189,7 @@ const Home: React.FC = () => {
             }
           }}
         ></Input>
-      </div>
+      </div> */}
       <div className={styles.list}>
         {projects?.length
           ? projects.map((project) => (
@@ -110,9 +199,9 @@ const Home: React.FC = () => {
                 onClick={() => navigate(`${PROJECT_NAV}/${project.id}`)}
               />
             ))
-          : !loading && <span>No projects</span>}
+          : ''}
       </div>
-      {loading && <Spinner />}
+      {(loading || updateLoading) && <Spinner />}
     </div>
   );
 };
