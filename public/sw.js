65516feefe135721260d4c3101f8f35ecaa2a05c
cyrbuzz <cyrbuzz@foxmail.com>
2023-10-27 17:52:25 +0800

    feat: service worker

---
+++ public/sw.js
@@ -0,0 +1,42 @@
+// Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+importScripts('https://static.subquery.network/sw/workbox-sw.js');
+
+workbox.setConfig({
+  modulePathPrefix: 'https://static.subquery.network/sw',
+});
+
+// https://developers.google.com/web/tools/workbox/reference-docs/latest/workbox.routing#registerRoute
+workbox.routing.registerRoute(
+  //,
+  /((localhost:3006)|((dev)|(kepler)\.thechaindata\.com)|(kepler.subquery.network))\/((dashboard)|(explorer)|(profile)|(indexer)|(delegator)|(consumer)|(swap)|(studio)).*/g,
+  // https://developers.google.com/web/tools/workbox/reference-docs/latest/workbox.strategies
+  new workbox.strategies.NetworkFirst({
+    cacheName: 'workbox:html',
+  }),
+);
+
+workbox.routing.registerRoute(
+  /\.js(?!\.json)(?=\?|$)/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'workbox:js',
+  }),
+);
+
+workbox.routing.registerRoute(
+  /\.css/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'workbox:css',
+  }),
+);
+
+workbox.routing.registerRoute(
+  // Cache image files
+  /.+\.(?:png|jpg|jpeg|svg|gif|ico)/,
+  // Use the cache if it's available
+  new workbox.strategies.StaleWhileRevalidate({
+    // Use a custom cache name
+    cacheName: 'workbox:image',
+  }),
+);
--- types/i18next.d.ts
+++ types/i18next.d.ts
@@ -1,8 +1,9 @@
 // Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { GetDictValue, TranslationKeys } from '@i18n';
+
 import 'i18next';
-import { TranslationKeys, GetDictValue } from '@i18n';
 
 // reference: https://www.i18next.com/overview/typescript#argument-of-type-defaulttfuncreturn-is-not-assignable-to-parameter-of-type-xyz
 declare module 'i18next' {
