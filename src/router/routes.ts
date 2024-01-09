0e4219937877dcb08fe95892a2bf84aa9d167b01
cyrbuzz <cyrbuzz@foxmail.com>
2024-01-09 13:50:09 +0800

    feat: disable for now

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -226,6 +226,6 @@ export const routers: BasicRouteType[] = [
 
   { path: '/consumer/*', component: React.lazy(() => import('../pages/consumer/index')) },
   { path: '/swap/*', component: React.lazy(() => import('../pages/swap/Swap')) },
-  { path: '/swapksqt/*', component: React.lazy(() => import('../pages/swapksqt')) },
+  { path: '/swapksqt/*', redirect: '/' },
   { path: '/studio/*', component: React.lazy(() => import('../pages/studio/index')) },
 ];
