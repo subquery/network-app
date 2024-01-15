535c04b62ed71b6a9ab4684041fc181239751480
cyrbuzz <cyrbuzz@foxmail.com>
2024-01-15 13:44:56 +0800

    chore: esapse

--- src/pages/swap/Swap.tsx
+++ src/pages/swap/Swap.tsx
@@ -226,7 +226,9 @@ const USDCToSqt = () => {
 
                 <div className="col-flex" style={{ justifyContent: 'flex-start' }}>
                   <SubqlTypography type="secondary">
-                    Only participants that have been KYC’d and whitelisted can participate in the SubQuery Kepler Swap. You check if your account has been KYC'd and whitelisted, please enter /kepler-ksqt-swap-kyc <your_wallet_address> in the #kepler-swap-support channel in our Discord.
+                    Only participants that have been KYC’d and whitelisted can participate in the SubQuery Kepler Swap.
+                    You check if your account has been KYC'd and whitelisted, please enter /kepler-ksqt-swap-kyc
+                    &lt;your_wallet_address&gt; in the #kepler-swap-support channel in our Discord.
                   </SubqlTypography>
                   <SubqlTypography type="secondary" style={{ marginTop: 20 }}>
                     If you’re new here, check out{' '}
