e66b9f0fda9a6c6165bdf02d12256ee0482c3d56
cyrbuzz <cyrbuzz@foxmail.com>
2024-02-28 18:36:17 +0800

    fix: link

--- src/const/bridge.ts
+++ src/const/bridge.ts
@@ -1,4 +1,4 @@
 // Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-export const BRIDGE_URL = 'https://bridge.base.org/deposit';
+export const BRIDGE_URL = 'https://app.subquery.network/bridge';
