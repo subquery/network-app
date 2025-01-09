e06ef5de77233c2ee246709bd903c6290d455412
Zark Alpha <534662506@qq.com>
2025-01-09 15:49:10 +0800

    Fix spelling errors: has mark -> has been marked

--- src/i18n/en/consumer.ts
+++ src/i18n/en/consumer.ts
@@ -72,8 +72,8 @@ const translation = {
       failureFetchPlans: 'Failed to get plans for Node Operator',
       notReadyToBePurchased: `This plan cannot be purchased until the Node Operator status is 'Ready'`,
     },
-    inactiveTemplate: 'This template has mark as inactive.',
-    inactiveTemplateTip: 'This template has mark as inactive, please delete it.',
+    inactiveTemplate: 'This template has been marked as inactive.',
+    inactiveTemplateTip: 'This template has been marked as inactive, please delete it.',
   },
   flexPlans: {
     project: 'project',
@@ -322,7 +322,7 @@ const translation = {
     accept: 'Accept',
     searchByDeploymentId: 'Search by deployment Id',
     alreadyAcceptedOffer: 'You have already accepted this offer',
-    offerInactive: 'This offer has mark as inactive',
+    offerInactive: 'This offer has been marked as inactive',
     acceptModal: {
       nonCriteriaData: 'There is no criteria data available',
       moveFromSummary: 'Move to the next step to check you meet the criteria for this offer.',
