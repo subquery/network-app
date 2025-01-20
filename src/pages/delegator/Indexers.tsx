4ad6cd909167468a95bcd10ab1d27f29e4bab001
cyrbuzz <cyrbuzz@foxmail.com>
2025-01-20 15:21:06 +0800

    feat: add more trigger

--- src/pages/delegator/Indexers.tsx
+++ src/pages/delegator/Indexers.tsx
@@ -5,13 +5,26 @@ import * as React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Outlet } from 'react-router';
 import { AppPageHeader } from '@components/AppPageHeader';
+import { ChatBoxPlanTextTrigger, Typography } from '@subql/components';
+
+import { useChatBoxStore } from 'src/stores/chatbox';
 
 export const Indexers: React.FC = () => {
   const { t } = useTranslation();
-
+  const { chatBoxRef } = useChatBoxStore();
   return (
     <>
-      <AppPageHeader title={t('indexer.indexers')} desc={t('allIndexers.desc')} />
+      <AppPageHeader
+        title={
+          <div className="col-flex" style={{ gap: '8px' }}>
+            <Typography variant="h5">{t('indexer.indexers')}</Typography>
+            <ChatBoxPlanTextTrigger triggerMsg="Which operator should I delegate to?" chatBoxInstance={chatBoxRef}>
+              Which operator should I delegate to?
+            </ChatBoxPlanTextTrigger>
+          </div>
+        }
+        desc={t('allIndexers.desc')}
+      />
       <Outlet></Outlet>
     </>
   );
