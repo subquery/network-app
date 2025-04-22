e97940fc19e09e6e637058e7cf1549738a6d1b7a
cyrbuzz <cyrbuzz@foxmail.com>
2025-04-22 15:41:11 +0800

    feat: delegation rewards

--- src/pages/account/Rewards/ClaimRewards.tsx
+++ src/pages/account/Rewards/ClaimRewards.tsx
@@ -17,6 +17,14 @@ import styles from './ClaimRewards.module.less';
 type Props = {
   account: string;
   indexers: Array<string>;
+  unhealthyIndexers?: {
+    unregisteredIndexers: {
+      id: string;
+    }[];
+    unclaimedIndexers: {
+      id: string;
+    }[];
+  };
   totalUnclaimed: string;
   unCliamedCountByIndexer: number;
   onClaimed?: (tx?: ContractReceipt) => void;
@@ -65,6 +73,88 @@ export const ClaimRewards: React.FC<Props> = ({
       text={text}
       buttonClassName={styles.claimButton}
       actions={[{ label: t('rewards.claim.button'), key: 'claim', style: { width: '100%' } }]}
+      onClick={handleClick}
+      rethrowWhenSubmit
+      renderContent={(onSubmit, _, isLoading, error) => {
+        return (
+          <div>
+            <Typography className={'errorText'}>{error}</Typography>
+            <div className={styles.btnContainer}>
+              <Button
+                onClick={onSubmit}
+                htmlType="submit"
+                shape="round"
+                size="large"
+                className={styles.submitBtn}
+                loading={isLoading}
+              >
+                {text.submitText}
+              </Button>
+            </div>
+          </div>
+        );
+      }}
+    />
+  );
+};
+
+export const ClaimRewardsForStake: React.FC<Props> = ({
+  indexers,
+  unhealthyIndexers,
+  unCliamedCountByIndexer,
+  onClaimed,
+}) => {
+  const { t } = useTranslation();
+  const { contracts } = useWeb3Store();
+
+  const unregisterdIndexers = React.useMemo(() => {
+    if (unhealthyIndexers?.unregisteredIndexers.length) {
+      return `Unregistered: ${unhealthyIndexers?.unregisteredIndexers.map((i) => i.id).join(', ')}`;
+    }
+
+    return '';
+  }, [unhealthyIndexers?.unregisteredIndexers]);
+
+  const unclaimedIndexers = React.useMemo(() => {
+    if (unhealthyIndexers?.unclaimedIndexers.length) {
+      return `Unclaimed: ${unhealthyIndexers?.unclaimedIndexers.map((i) => i.id).join(', ')}`;
+    }
+
+    return '';
+  }, [unhealthyIndexers?.unclaimedIndexers]);
+
+  const text = React.useMemo(() => {
+    return {
+      title: t('rewards.claim.title'),
+      steps: [t('rewards.claim.step1'), t('indexer.confirmOnMetamask')],
+      description: `The rewards will directly be staked to the operator. Unregisterd operators and unclaimed reward operators will be skipped. 
+      ${unregisterdIndexers} ${unclaimedIndexers}`,
+      submitText: t('rewards.claim.submit'),
+      failureText: 'Sorry, failed to claim rewards',
+    };
+  }, [unregisterdIndexers, unclaimedIndexers]);
+
+  const handleClick = async () => {
+    assert(contracts, 'Contracts not available');
+    if (unCliamedCountByIndexer > 100) {
+      openNotification({
+        type: 'info',
+        description: 'unclaimed rewards are more than 100, need to claim more than once.',
+        duration: 5,
+      });
+    }
+    const pendingTx = await contracts.stakingManager.batchStakeReward(indexers);
+    const recepit = await pendingTx.wait();
+    await onClaimed?.(recepit);
+    return pendingTx;
+  };
+
+  return (
+    <TransactionModal
+      variant="button"
+      text={text}
+      buttonClassName={styles.claimButton}
+      actions={[{ label: 'Claim rewards to stake', key: 'claim', style: { width: '100%' } }]}
       onClick={handleClick}
       rethrowWhenSubmit
       renderContent={(onSubmit, _, isLoading, error) => {
